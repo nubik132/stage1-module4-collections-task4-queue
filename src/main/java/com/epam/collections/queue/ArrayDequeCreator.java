1ab4b86dbf72412c1b55e71da4cff91db98b907b
nubik132 <mister.nubik@list.ru>
2023-11-30 14:27:55 +0300

    feat: complete task

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,25 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+
+        deque.offer(firstQueue.poll());
+        deque.offer(firstQueue.poll());
+        deque.offer(secondQueue.poll());
+        deque.offer(secondQueue.poll());
+
+        while (!firstQueue.isEmpty() && !secondQueue.isEmpty()){
+            firstQueue.offer(deque.pollLast());
+            deque.offer(firstQueue.poll());
+            deque.offer(firstQueue.poll());
+            secondQueue.offer(deque.pollLast());
+            deque.offer(secondQueue.poll());
+            deque.offer(secondQueue.poll());
+        }
+        return deque;
     }
 }
+//
+//
+
+// 7 6 8 3 1 2 5 5
