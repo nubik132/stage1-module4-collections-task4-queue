e16719a7071467950808658be424811482fc84fb
nubik132 <mister.nubik@list.ru>
2023-11-30 14:02:28 +0300

    feat: complete task

--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -1,9 +1,17 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 
 public class PriorityQueueCreator {
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
+        ArrayList<String> list = new ArrayList<>();
+        list.addAll(firstList);
+        list.addAll(secondList);
+        PriorityQueue<String> queue = new PriorityQueue<>(Comparator.reverseOrder());
+        queue.addAll(list);
+        return queue;
     }
 }
