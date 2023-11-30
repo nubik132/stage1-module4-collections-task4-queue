62aa535f6de0052071e09e70f4396f2b5dc7c83f
nubik132 <mister.nubik@list.ru>
2023-11-30 15:46:20 +0300

    feat: complete task

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,27 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> numericList = new LinkedList<>();
+        for (int i = 0; i < numberOfDishes; i++) {
+            numericList.add(i + 1);
+        }
+
+        List<Integer> list = new ArrayList<>();
+        int i = everyDishNumberToEat - 1;
+        if (numericList.isEmpty()){
+            return list;
+        }
+        list.add(numericList.remove(i--));
+        while (!numericList.isEmpty()){
+            i = (i + everyDishNumberToEat) % numericList.size();
+            list.add(numericList.remove(i--));
+        }
+        return list;
     }
 }
