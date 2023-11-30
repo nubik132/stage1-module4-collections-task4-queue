7a0c7b392a3aa0e902d5ed756a67388853b938de
nubik132 <mister.nubik@list.ru>
2023-11-30 15:47:21 +0300

    fix: delete unused import

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -3,7 +3,6 @@ package com.epam.collections.queue;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
